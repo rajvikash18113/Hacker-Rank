adbc0a07a44a1fdd7d7023608855e2e6019aa152
Vikash Kumar <rajvikash18113@gmail.com>
2024-09-17 19:51:59 +0530

    Create 002_input_and_output.cpp

---
+++ 002_input_and_output.cpp
@@ -0,0 +1,17 @@
+/*
+    Task
+    Read  numbers from stdin and print their sum to stdout.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    int sum = a + b + c;
+    cout << sum;
+    return 0;
+}
