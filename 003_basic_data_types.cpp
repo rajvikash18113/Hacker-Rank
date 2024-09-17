108b73798166f298834ccf47dd94638b9b78cc69
Vikash Kumar <rajvikash18113@gmail.com>
2024-09-17 19:52:08 +0530

    Create 003_basic_data_types.cpp

---
+++ 003_basic_data_types.cpp
@@ -0,0 +1,38 @@
+/*
+Input Format
+Input consists of the following space-separated values: int, long, char, float, and double, respectively.
+
+Sample Input
+3 12345678912345 a 334.23 14049.30493
+
+Sample Output
+3
+12345678912345
+a
+334.230
+14049.304930000
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i;
+    long l;
+    char ch;
+    float f;
+    double d;
+
+    cin >> i >> l >> ch >> f >> d;
+    
+    
+    cout << i << endl;
+    cout << l << endl;
+    cout << ch << endl;
+    cout << fixed << setprecision(3) << f << endl;
+    cout << fixed << setprecision(9) << d << endl;
+}
+
+
+/* NOTE : working on hacker rank compiler but not working in VS Code*/
