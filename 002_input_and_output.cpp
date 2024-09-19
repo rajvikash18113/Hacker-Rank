b4db53bdbcce33adac6f9ed6ae565139e03793b7
Vikash Kumar <rajvikash18113@gmail.com>
2024-09-19 17:57:11 +0530

    Update 002_input_and_output.cpp

--- 002_input_and_output.cpp
+++ 002_input_and_output.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int main()
 {
     int a, b, c; // Creating Variable 
-    cin >> a >> b >> c; 
+    cin >> a >> b >> c; // Taking input from user
 
     int sum = a + b + c;
     cout << sum;
