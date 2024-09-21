5d5dc8860856cd51cf4006d00878b3a89b1c1ff8
Vikash Kumar <rajvikash18113@gmail.com>
2024-09-21 18:15:12 +0530

    Update 004_conditional_statement.cpp

--- 004_conditional_statement.cpp
+++ 004_conditional_statement.cpp
@@ -1,5 +1,5 @@
 /*
-Given a positive integer , do the following:
+ Given a positive integer , do the following:
 
  If (1 <= n <= 9), print the lowercase English word corresponding to the number (e.g., one for 1, two for 2, etc.).
  If (n > 9), print Greater than 9
