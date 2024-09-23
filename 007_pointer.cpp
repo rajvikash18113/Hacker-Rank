e5359bb6c1ab2f3fab25893195ef92c84485e9d8
Vikash Kumar <rajvikash18113@gmail.com>
2024-09-23 11:15:08 +0530

    Create 007_pointer.cpp

---
+++ 007_pointer.cpp
@@ -0,0 +1,34 @@
+/*
+The function is declared with a void return type, so there is no value to return.
+Modify the values in memory so that a contains their sum and b contains their absoluted difference.
+sum = |a+b|
+diff = |a-b|
+*/
+
+#include <iostream>
+using namespace std;
+
+// creating a function named modify with void return type
+void modify(int *p1, int *p2) // taking 2 pointer integer as parameter of modify function
+{
+    int sum = *p1 + *p2;       
+    int diff = *p1 - *p2;
+    *p1 = sum;              // containing value of sum in 1st pointer
+    *p2 = abs(diff);        // containing absolute value of difference in 2nd pointer
+                            // abs() function is used to 
+
+    // if(diff < 0)
+    // {
+    //     diff = -(diff);
+    // }
+    // *p2 = (diff);
+}
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    modify(&a, &b);
+    cout << a << endl;
+    cout << b << endl;
+}
